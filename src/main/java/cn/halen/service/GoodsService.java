2afe928cd861125402bba5c9b57f9383c70e8030
halen <zhangzhiwei20@163.com>
2013-10-25 20:41:57 +0800

    bugfix

--- src/main/java/cn/halen/service/GoodsService.java
+++ src/main/java/cn/halen/service/GoodsService.java
@@ -66,7 +66,12 @@ public class GoodsService {
                 continue;
             }
             List<Sku> skuList = response.getSkus();
-            logger.info("skuList from top size is {}", skuList.size());
+            if(skuList==null || skuList.size()==0) {
+                logger.info("query shop : {}, skuId : {} , have no item!", shop.getSellerNick(), skuId);
+            } else {
+                logger.debug("query shop : {}, skuId : {}, skuList from top size is {}", shop.getSellerNick(), skuId,
+                        skuList.size());
+            }
             if(skuList != null && skuList.size() > 0) {
                 long onlineQuantity = Math.round((mySku.getQuantity() - mySku.getLock_quantity()
                         - mySku.getManaual_lock_quantity()) * shop.getRate());
