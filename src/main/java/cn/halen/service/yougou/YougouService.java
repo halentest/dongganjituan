42b1c8dfc0b958afa1c8c7d3fe1ed48e7ab7bac6
halen <zhangzhiwei20@163.com>
2013-12-31 15:32:51 +0800

    yougou

--- src/main/java/cn/halen/service/yougou/YougouService.java
+++ src/main/java/cn/halen/service/yougou/YougouService.java
@@ -18,15 +18,14 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 /**
  * User: hzhang
@@ -37,6 +36,7 @@ import java.util.List;
 public class YougouService {
     private static final String yougouUrl = "http://183.62.162.119:80/mms/api.sc";
     private static final String yougouAppKey = "SP20120412312125";
+    private static final String yougouAppSecret = "0123456789";
 
     @Autowired
     private TopConfig topConfig;
@@ -48,7 +48,9 @@ public class YougouService {
 
     public static void main(String[] args) throws MalformedURLException, UnsupportedEncodingException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
-        String timestamp = FastDateFormat.getInstance("yyyyMMdd HH:mm:ss").format(new Date());
+        String timestamp = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss").format(new Date());
+        System.out.println(timestamp);
+        List<NameValuePair> list = new ArrayList<NameValuePair>();
         StringBuilder urlBuilder = new StringBuilder();
         urlBuilder.append(yougouUrl)
                 .append("?method=yougou.inventory.query")
@@ -56,8 +58,31 @@ public class YougouService {
                 .append("&app_key=").append(yougouAppKey)
                 .append("&sign_method=MD5")
                 .append("&app_version=1.0")
-                .append("&format=xml");
+                .append("&format=xml")
+                .append("&page_index=1")
+                .append("&page_size=5");
+        list.add(new BasicNameValuePair("method", "yougou.inventory.query"));
+        list.add(new BasicNameValuePair("timestamp", timestamp));
+        list.add(new BasicNameValuePair("app_key", yougouAppKey));
+        list.add(new BasicNameValuePair("sign_method", "MD5"));
+        list.add(new BasicNameValuePair("app_version", "1.0"));
+        list.add(new BasicNameValuePair("format", "xml"));
+        list.add(new BasicNameValuePair("page_index", "1"));
+        list.add(new BasicNameValuePair("page_size", "5"));
+        Collections.sort(list, new Comparator<NameValuePair>() {
+            @Override
+            public int compare(NameValuePair o1, NameValuePair o2) {
+                return o1.getName().compareToIgnoreCase(o2.getName());
+            }
+        });
+        StringBuilder sourceBuilder = new StringBuilder(yougouAppSecret);
+        for(NameValuePair p : list) {
+            sourceBuilder.append(p.getName()).append(p.getValue());
+        }
+        String md5 = DigestUtils.md5DigestAsHex(sourceBuilder.toString().getBytes());
+        urlBuilder.append("&sign=").append(md5);
         HttpGet get = new HttpGet(urlBuilder.toString());
+        System.out.println(urlBuilder.toString());
         BufferedReader reader = null;
         StringBuilder builder = new StringBuilder();
         try {
